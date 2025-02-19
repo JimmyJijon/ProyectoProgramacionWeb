64279c6633613ee8c07c05aa168f8c05755c7ca9
JimmyJijon <jajijon2@tes.edu.ec>
2025-02-19 14:58:43 -0500

    se termina botones del frontend

--- gestion-estudiantes-frontend/src/components/HomePageComponent.js
+++ gestion-estudiantes-frontend/src/components/HomePageComponent.js
@@ -7,25 +7,27 @@ function HomePageComponent() {
 
   const botones = [
     { icono: "/actividad-extracurricular.png", texto: "Actividad extracurricular" },
-    { icono: "/aulavirtual.png", texto: "Aula virtual" },
+    { icono: "/aulavirtual.png", texto: "Aula virtual", link: "https://tesonline.academicok.com/login/index.php"},
     { icono: "/bolsalaboral.png", texto: "Bolsa laboral" },
     { icono: "/botondepagos.png", texto: "Botón de pagos" },
     { icono: "/calendario.png", texto: "Calendario de actividades de la institución"},
-    { icono: "/facturasemitidas.png", texto: "Facturas emitidas"},
-    { icono: "", texto: "Ficha socioeconómica"},
-    { icono: "", texto: "Hoja de vida del estudiante"},
-    { icono: "", texto: "Matriculación online"},
-    { icono: "", texto: "Mensajeria interna"},
-    { icono: "", texto: "Cronograma de mis materias y profesores"},
-    { icono: "", texto: "Datos personales y familiares medicos"},
-    { icono: "", texto: "Mis rubros y deuda"},
-    { icono: "", texto: "Ver mis horarios de clases programados para el periodo"},
-    { icono: "", texto: "Notas y asistencia en el periodo lectivo"},
-    { icono: "", texto: "Resumen de mis planes contratados"},
-    { icono: "", texto: "Notas obtenidas en la carrera"},
-    { icono: "", texto: "Solicitudes de certificados online"},
-    { icono: "", texto: "Videos de inducción institucional"},
-    { icono: "", texto: "Vinculación con la comunidad"},
+    { icono: "/evaluaciondemisprofesores.png", texto: "Evaluación del alumno a sus docentes"},
+    { icono: "/facturas.png", texto: "Facturas emitidas"},
+    { icono: "/fichasocioeconomica.png", texto: "Ficha socioeconómica"},
+    { icono: "hojadevida.png", texto: "Hoja de vida del estudiante"},
+    { icono: "matriculaciononline.png", texto: "Matriculación online"},
+    { icono: "mensajeriainterna.png", texto: "Mensajeria interna"},
+    { icono: "micronograma.png", texto: "Cronograma de mis materias y profesores"},
+    { icono: "mifichamedica.png", texto: "Datos personales y familiares medicos"},
+    { icono: "mimalla.png", texto: "Ver mi cumplimiento de malla"},
+    { icono: "misfinanzas.png", texto: "Mis rubros y deuda"},
+    { icono: "mishorarios.png", texto: "Ver mis horarios de clases programados para el periodo"},
+    { icono: "mismaterias.png", texto: "Notas y asistencia en el periodo lectivo"},
+    { icono: "misplanes.png", texto: "Resumen de mis planes contratados"},
+    { icono: "recordacademico.png", texto: "Notas obtenidas en la carrera"},
+    { icono: "solicituddecertificados.png", texto: "Solicitudes de certificados online"},
+    { icono: "videosdeinduccion.png", texto: "Videos de inducción institucional"},
+    { icono: "vinculacioncomunidad.png", texto: "Vinculación con la comunidad"},
 
     // ... y así con los demás
   ];
@@ -40,18 +42,31 @@ function HomePageComponent() {
         <div className='FotoDelEst'>
         <img className= "imgEst"src="/userUnknown.webp"></img>
         </div>
-        <EstudianteCard estudianteId={2}/>
+        <EstudianteCard estudianteId={9}/>
       </div>
 
       {/* Contenedor para los “botones” */}
       <div className="botones-container">
-        {botones.map((item, index) => (
-          <div key={index} className="boton-item">
-            <img src={item.icono} alt={item.texto} className="icono-boton" />
-            <span>{item.texto}</span>
-          </div>
-        ))}
+  {botones.map((item, index) => (
+    item.link ? (
+      <a 
+        key={index} 
+        href={item.link} 
+        target="_blank" 
+        rel="noopener noreferrer" 
+        className="boton-item"
+      >
+        <img src={item.icono} alt={item.texto} className="icono-boton" />
+        <span>{item.texto}</span>
+      </a>
+    ) : (
+      <div key={index} className="boton-item">
+        <img src={item.icono} alt={item.texto} className="icono-boton" />
+        <span>{item.texto}</span>
       </div>
+    )
+  ))}
+</div>
 
     </div>
   );
